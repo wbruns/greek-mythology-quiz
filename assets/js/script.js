d50ad889ca6fe228efc1b134ecae54706f19b73b
wbruns <wbruns07@gmail.com>
2022-01-23 23:09:11 -0600

    added a timer. for loop for showing questions is now broken and looping forever

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,25 +8,45 @@ quizQA = {
     "Zeus and Aphrodite", "Poseidon and Athena", "Hades and Persephone", "Zeus and Hera"]
 };
 
+// 2 minute initial timer
+var timeLeft = 120;
 
 var quizStart = function() {
   // display the quiz
   $("#quiz").show(1000);
   
-  // loop over questions
-  for (i = 0; i < quizQA.questions.length; i++) {
-    console.log(quizQA.questions[i]);
-    // loop over answers
-    for (i = 0; i < 4; i++) {
-      console.log(quizQA.answers[i]);
+  // while loop to account for remaining time
+  while (timeLeft > 0) {
+    debugger;
+    // loop over questions
+    for (i = 0; i < quizQA.questions.length; i++) {
+      $("#quiz-content").append("<h2>" + quizQA.questions[i] + "</h2>");
+      console.log(quizQA.questions[i]);
+      // loop over answers
+      for (i = 0; i < 4; i++) {
+        $("#quiz-content").append("<button class='mt-2'>" + quizQA.answers[i] + "</button>");
+        console.log(quizQA.answers[i]);
+      }
     }
   }
+  
 };
+
+
 
 // when start button is clicked
 $("#start").click(function() {
   // hide the intro
   $("#intro").hide("slow");
+  
+
+  // timer for the quiz, updates every second
+  var timer = setInterval(function() {
+    document.getElementById("timer").innerHTML = "Time: " + timeLeft;
+    console.log(timeLeft);
+    timeLeft--;
+  }, 1000);
+
   // start the quiz
   quizStart();
 });
